c985b22c90ef5f622c5fdafa83c1520c82c25dd3
unknown <Administrador@LAB2M29.unifil.br>
2018-06-04 20:36:07 -0300

    new class

---
+++ problemaAspirador.py
@@ -0,0 +1,25 @@
+if __name__ == "__main__":
+    print("Esta classe não é para ser rodada")
+    exit(-1)
+
+class ProblemaAspirador():
+
+    @staticmethod
+    def getEstadoInicial():
+        pass
+    
+    @staticmethod
+    def acoesPossiveisEm(estado):
+        pass
+    
+    @staticmethod
+    def estadoResultanteDe(acao, estado)
+        pass
+
+    @staticmethod
+    def isObjetivo(estado):
+        pass
+    
+    @staticmethod
+    def calcularCustoAcao(estado, acao, estadoResultante):
+        pass
